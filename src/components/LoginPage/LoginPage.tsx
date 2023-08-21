c7a9d6eefc5fee288f82acd9f97573a49f796542
Isaac Vera <isaacvera37@icloud.com>
2023-08-20 23:36:00 -0700

    installed: sass / feat: invalid login message

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,21 +1,26 @@
 import React, {useState, ChangeEvent} from 'react';
 
 const LoginPage = () => {
-    const [userEmail, setUserEmail] = useState<string>('');
-    const [userPassword, setUserPassword] = useState<string>('');
+    const [enteredEmail, setEnteredEmail] = useState<string>('');
+    const [enteredPassword, setEnteredPassword] = useState<string>('');
 
-    //todo: Login Handler and styles
     const loginHandler = () => {
-       console.log(userEmail);
-       console.log(userPassword);
+        if (isValid()) {
+           return true; 
+        } 
+        return false;
     } 
 
     const emailHandler = (event: ChangeEvent<HTMLInputElement>): void => {
-        setUserEmail(event.target.value);
+        setEnteredEmail(event.target.value);
     }
 
     const passwordHandler = (event: ChangeEvent<HTMLInputElement>): void => {
-        setUserPassword(event.target.value);
+        setEnteredPassword(event.target.value);
+    }
+
+    const isValid = () => {
+        return (enteredEmail.includes('@') && enteredPassword.trim().length > 5);
     }
 
 
@@ -23,11 +28,17 @@ const LoginPage = () => {
     <div>
         <h2>Admin Login</h2>
         <h3>Email</h3>
-        <input type={'text'} value={userEmail} onChange={emailHandler}/>
+        <input type={'text'} value={enteredEmail} onChange={emailHandler}/>
         <h3>Password</h3>
-        <input type={'text'} value={userPassword} onChange={passwordHandler}/>
+        <input type={'text'} value={enteredPassword} onChange={passwordHandler}/>
 
         <button onClick={loginHandler}>Login</button>
+
+        {loginHandler() ? <div>
+            <div>Invalid Input</div>
+            <div>"Please enter a valid email address containing the '@' symbol,
+             and ensure your password is at least 6 characters long."</div>
+        </div>: null}
     </div>)
 }
 
